34a4a417a8cbcb0b531233498e990ad69ae92725
kj <redacted>
2016-05-19 13:28:00 -0400

    bumped version

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name = 'taskthreader',
     py_modules=['taskthreader'],
-    version = '0.1',
+    version = '0.1.1',
     description = 'Python task threading helper.',
     author = 'KJ',
     author_email = '<see github for support>',
     url = 'https://github.com/jdotpy/taskthreader',
-    download_url = 'https://github.com/jdotpy/taskthreader/tarball/0.1',
+    download_url = 'https://github.com/jdotpy/taskthreader/tarball/0.1.1',
     keywords = ['tools'],
     classifiers = [],
 )
