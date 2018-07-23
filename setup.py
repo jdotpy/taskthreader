6b5cccf1f6dbe31399cefc0a27bd2c7cbb84e612
KJ <jdotpy@users.noreply.github.com>
2018-07-23 10:56:17 -0700

    Fixed setup.py email

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     version = '1.0.0',
     description = 'Python task threading helper.',
     author = 'KJ',
-    author_email = '<see github for support>',
+    author_email = 'jdotpy@users.noreply.github.com',
     url = 'https://github.com/jdotpy/taskthreader',
     download_url = 'https://github.com/jdotpy/taskthreader/tarball/0.1.1',
     keywords = ['tools'],
